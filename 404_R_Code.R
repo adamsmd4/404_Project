09803ca122562895d1903e935bc614b030b3fd17
kleinjb25 <kleinjb@miamioh.edu>
2025-04-24 15:35:30 -0400

    made it so user can search for game in dynamic dropdown

--- 404_R_Code.R
+++ 404_R_Code.R
@@ -1,8 +1,6 @@
+library(shiny)
+library(dplyr)
 # downloading and cleaning the data
-remotes::install_github("thebioengineer/tidytuesdayR")
-tuesdata <- tidytuesdayR::tt_load('2021-03-16')
-games <- tuesdata$games
-
 games <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2021/2021-03-16/games.csv')
 games <- games %>%
   filter(
@@ -12,7 +10,8 @@ games <- games %>%
   )
 
 str(games)
-hist(games$avg, games$year) # this line doesn't work, but since it just makes a histogram,
+head(games$gamename)
+#hist(games$avg, games$year) # this line doesn't work, but since it just makes a histogram,
 # its not needed (unless we want to make a histogram)
 
 # plots to make:
@@ -21,3 +20,35 @@ hist(games$avg, games$year) # this line doesn't work, but since it just makes a 
 # then it shows a line chart for that metric over all 12 years.
 # could also do a mix of 1 and 2 where user searches for game, picks month / year, 
 # and there's a scatterplot with x-axis = average players, y-axis = peak players, and bubble size = gain
+
+## Plot 2: user searches for game, chooses metric,
+# then it shows a line chart for that metric over all 12 years.
+ui <- fluidPage(
+  selectizeInput("gameQuery", "Search for a game:", choices=NULL,
+                 options=list(placeholder="search here...", maxItems=1, openOnFocus=T)),
+  textOutput("selectedGame")
+)
+
+server <- function(input, output, session) {
+  observe({
+    updateSelectizeInput(session, "gameQuery", 
+                         choices = games$gamename, 
+                         server = TRUE)
+  })
+  observe({
+    query <- input$gameQuery
+    if (query != "" && query != input$gameQuery) {
+      filtered <- games %>% 
+        filter(grepl(query, gamename, ignore.case=T)) %>%
+        pull(gamename) %>% unique()
+      if (length(filtered) > 0)
+        updateSelectizeInput(session, "gameQuery", choices = filtered, server = TRUE)
+      else
+        updateSelectizeInput(session, "gameQuery", choices = games$gamename, server = TRUE)
+    } 
+  })
+  output$selectedGame <- renderText({
+    paste("You selected:", input$gameQuery)
+  })
+}
+shinyApp(ui, server)
